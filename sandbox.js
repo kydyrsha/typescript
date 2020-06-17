0c8a1d337ccba31922c5f9fca5f7a52bd7b40295
kydyrsha <sanatkydyrsha@gmail.com>
2020-06-17 12:38:20 +0600

    type basics

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,10 @@
-var character = 'mario';
+var character = 'mario'; // string
+var age = 30; // number
+var isBlackBelt = false; // boolean
+// character = 20; // can't do like this
+character = "luigi"; // we must change only the VALUE not the TYPE
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7));
--- sandbox.ts
+++ sandbox.ts
@@ -1,1 +1,13 @@
-let character = 'mario';
+let character = 'mario'; // string
+let age = 30; // number
+let isBlackBelt = false; // boolean
+
+// character = 20; // can't do like this
+character = "luigi" // we must change only the VALUE not the TYPE
+isBlackBelt = true;
+
+const circ = (diameter:number) => {
+  return diameter * Math.PI
+}
+
+console.log(circ(7))
