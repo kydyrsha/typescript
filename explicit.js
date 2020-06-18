0a95ee7539c4bceb9e9695a61774368c80a1a183
kydyrsha <sanatkydyrsha@gmail.com>
2020-06-18 08:54:50 +0600

    explicit types of ts

---
+++ explicit.js
@@ -0,0 +1,22 @@
+// explicit Type
+var character;
+var age;
+var isLoggedIn;
+age = 30;
+isLoggedIn: true;
+//arrays
+var ninjas;
+ninjas = ['yoshi', 'sanat'];
+ninjas.push('shaun'); // we can do it like, but if array is EMPTY WE CAN'T
+// union types
+var mixed = [];
+mixed.push('Hello');
+mixed.push(20);
+var uid;
+uid = '123';
+uid = 123;
+// objects
+var ninjaOne;
+ninjaOne = { name: 'yoshi', age: 30 };
+ninjaOne = []; // allowed
+var ninjaTwo;
