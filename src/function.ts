85d32fef6644dd374213a5b616088f403bbc9231
kydyrsha <sanatkydyrsha@gmail.com>
2020-06-18 15:10:25 +0600

    function type

--- src/function.ts
+++ src/function.ts
@@ -1,14 +1,27 @@
-let greet: Function;
+// let greet: Function;
+//
+// // greet = 'hello';
+//
+// greet = () => {
+//   console.log('hello,again');
+// }
+//
+// const add = (a:number, b:number, c?: number | string) => {
+//   console.log(a + b);
+//   console.log(c); // optional
+// }
+//
+// add(5,10);
 
-// greet = 'hello';
+// Type Aliases
 
-greet = () => {
-  console.log('hello,again');
+type StringOrNum = string | number;
+type objWithName = { name: string, uid: StringOrNum};
+
+const logDetails = (uid: StringOrNum, item: string) => {
+  console.log ('${item} has a uid of ${uid}');
 }
 
-const add = (a:number, b:number, c?: number | string) => {
-  console.log(a + b);
-  console.log(c); // optional
+const great = (user: {name: objWithName}) => {
+  console.log ('${user.name} says hello');
 }
-
-add(5,10);
