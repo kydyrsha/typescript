e7339a949633e1293c7b064a8fd6562b3bfff7d3
kydyrsha <sanatkydyrsha@gmail.com>
2020-06-19 16:30:25 +0600

    new file app.ts

--- src/function.js
+++ src/function.js
@@ -1,20 +1,20 @@
-// let greet: Function;
-//
-// // greet = 'hello';
-//
-// greet = () => {
-//   console.log('hello,again');
-// }
-//
-// const add = (a:number, b:number, c?: number | string) => {
-//   console.log(a + b);
-//   console.log(c); // optional
-// }
-//
-// add(5,10);
-var logDetails = function (uid, item) {
-    console.log('${item} has a uid of ${uid}');
-};
-var great = function (user) {
-    console.log('${user.name} says hello');
-};
+// // let greet: Function;
+// //
+// // // greet = 'hello';
+// //
+// // greet = () => {
+// //   console.log('hello,again');
+// // }
+// //
+// // const add = (a:number, b:number, c?: number | string) => {
+// //   console.log(a + b);
+// //   console.log(c); // optional
+// // }
+// //
+// // add(5,10);
+// var logDetails = function (uid, item) {
+//     console.log('${item} has a uid of ${uid}');
+// };
+// var great = function (user) {
+//     console.log('${user.name} says hello');
+// };
--- src/function.ts
+++ src/function.ts
@@ -1,27 +1,28 @@
-// let greet: Function;
+// // let greet: Function;
+// //
+// // // greet = 'hello';
+// //
+// // greet = () => {
+// //   console.log('hello,again');
+// // }
+// //
+// // const add = (a:number, b:number, c?: number | string) => {
+// //   console.log(a + b);
+// //   console.log(c); // optional
+// // }
+// //
+// // add(5,10);
 //
-// // greet = 'hello';
+// // Type Aliases
+// type StringOrNum = string | number;
+// type objWithName = { name: string, uid: StringOrNum};
 //
-// greet = () => {
-//   console.log('hello,again');
+// const logDetails = (uid: StringOrNum, item: string) => {
+//   console.log ('${item} has a uid of ${uid}');
 // }
 //
-// const add = (a:number, b:number, c?: number | string) => {
-//   console.log(a + b);
-//   console.log(c); // optional
+// const great = (user: {name: objWithName}) => {
+//   console.log ('${user.name} says hello');
 // }
 //
-// add(5,10);
-
-// Type Aliases
-
-type StringOrNum = string | number;
-type objWithName = { name: string, uid: StringOrNum};
-
-const logDetails = (uid: StringOrNum, item: string) => {
-  console.log ('${item} has a uid of ${uid}');
-}
-
-const great = (user: {name: objWithName}) => {
-  console.log ('${user.name} says hello');
-}
+// // Function Signatures
