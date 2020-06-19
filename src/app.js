0e2cd7882028fec2855b9620bc464be1d1af9f81
kydyrsha <sanatkydyrsha@gmail.com>
2020-06-19 16:54:13 +0600

    getting values

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-console.log('hello');
+// const anchor = document.querySelector('a')!;
+//
+// // if (anchor) {
+// //   console.log(anchor.href);
+// // }
+//
+// console.log(anchor.href);
+//
+//
+// !mark "I KNOW THAT NOT NULL | ITS NOT ERROR"
+// const form = document.querySelector('form')!;
+var form = document.querySelector('.new-item-form'); // as HTMLBlaBlaElemnt will as IT type
+// console.log(form.children);
+// inputs
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#toform');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault(); // prevents the page refreshing\
+    console.log(type.value, tofrom.value, details.value, amount.value);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,31 @@
-console.log('hello');
+// const anchor = document.querySelector('a')!;
+//
+// // if (anchor) {
+// //   console.log(anchor.href);
+// // }
+//
+// console.log(anchor.href);
+//
+//
+// !mark "I KNOW THAT NOT NULL | ITS NOT ERROR"
+// const form = document.querySelector('form')!;
+const form = document.querySelector('.new-item-form') as HTMLFormElement; // as HTMLBlaBlaElemnt will as IT type
+// console.log(form.children);
+
+// inputs
+
+const type = document.querySelector('#type')! as HTMLSelectElement;
+const tofrom = document.querySelector('#toform')! as HTMLInputElement;
+const details = document.querySelector('#details')! as HTMLInputElement;
+const amount = document.querySelector('#amount')! as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault(); // prevents the page refreshing\
+
+  console.log(
+    type.value,
+    tofrom.value,
+    details.value,
+    amount.value
+  );
+})
