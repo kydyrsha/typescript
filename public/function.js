c70e8a00df4e30de352e1a6f1d8865d6cf05a6e5
kydyrsha <sanatkydyrsha@gmail.com>
2020-06-19 16:31:52 +0600

    comment and app.js added

--- public/function.js
+++ public/function.js
@@ -1,17 +1,17 @@
-var greet;
-// greet = 'hello';
-greet = function () {
-    console.log('hello,again');
-};
-var add = function (a:number, b:number, c:number | string = 10): void {
-    console.log(a + b);
-    console.log(c); // optional
-};
-add(5, 10);
-
-
-const minus = (a:number, b: number): number => {
-  return = a + b;
-}
-
-let result = minus(10,7);
+// var greet;
+// // greet = 'hello';
+// greet = function () {
+//     console.log('hello,again');
+// };
+// var add = function (a:number, b:number, c:number | string = 10): void {
+//     console.log(a + b);
+//     console.log(c); // optional
+// };
+// add(5, 10);
+//
+//
+// const minus = (a:number, b: number): number => {
+//   return = a + b;
+// }
+//
+// let result = minus(10,7);
