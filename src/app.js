965e2706dd8e8a2732dab2931723adaa74ea5f48
kydyrsha <sanatkydyrsha@gmail.com>
2020-07-01 11:47:22 +0600

    modules added, however some issues with the ES

--- src/app.js
+++ src/app.js
@@ -1,34 +1,8 @@
-// const anchor = document.querySelector('a')!;
-//
-// // if (anchor) {
-// //   console.log(anchor.href);
-// // }
-//
-// console.log(anchor.href);
-//
-//
-// !mark "I KNOW THAT NOT NULL | ITS NOT ERROR"
-// const form = document.querySelector('form')!;
-//classes
-var Invoice = /** @class */ (function () {
-    // readonly client:string;
-    // private details: string;
-    // public amount: number;
-    // public user modifier, default one
-    // private has no access, only can be used inside CLASS
-    // only can read inside/outside the class
-    function Invoice(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    Invoice.prototype.format = function () {
-        return this.client + " owes \u00A3$(this.amount) for $(this.details)";
-    };
-    return Invoice;
-}());
-var invOne = new Invoice("mario", "work on the mario website", 250);
-var invTwo = new Invoice("luigi", "work on the luigi website", 300);
+"use strict";
+exports.__esModule = true;
+var invoice_js_1 = require("./classes/invoice.js");
+var invOne = new invoice_js_1.Invoice("mario", "work on the mario website", 250);
+var invTwo = new invoice_js_1.Invoice("luigi", "work on the luigi website", 300);
 console.log(invOne, invTwo);
 var invoices = [];
 invoices.push(invOne);
@@ -36,8 +10,8 @@ invoices.push(invTwo);
 invoices.forEach(function (inv) {
     console.log(inv.client, inv.amount, inv.format());
 });
-invOne.client = 'sanat';
-invTwo.amount = 333;
+// invOne.client = 'sanat';
+// invTwo.amount = 333;
 console.log(invoices);
 var form = document.querySelector('.new-item-form'); // as HTMLBlaBlaElemnt will as IT type
 // console.log(form.children);
--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,5 @@
-// const anchor = document.querySelector('a')!;
-//
-// // if (anchor) {
-// //   console.log(anchor.href);
-// // }
-//
-// console.log(anchor.href);
-//
-//
-// !mark "I KNOW THAT NOT NULL | ITS NOT ERROR"
-// const form = document.querySelector('form')!;
-//classes
+import { Invoice } from './classes/invoice.js'
 
-class Invoice {
-  // readonly client:string;
-  // private details: string;
-  // public amount: number;
-
-  // public user modifier, default one
-  // private has no access, only can be used inside CLASS
-  // only can read inside/outside the class
-
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount:number
-  ) {}
-
-  format() {
-    return `${this.client} owes £$(this.amount) for $(this.details)`;
-  }
-}
 
 const invOne = new Invoice (`mario`, `work on the mario website`, 250);
 const invTwo = new Invoice (`luigi`, `work on the luigi website`, 300);
@@ -44,8 +14,8 @@ invoices.forEach(inv => {
   console.log(inv.client, inv.amount, inv.format());
 })
 
-invOne.client = 'sanat';
-invTwo.amount = 333;
+// invOne.client = 'sanat';
+// invTwo.amount = 333;
 
 console.log(invoices)
 const form = document.querySelector('.new-item-form') as HTMLFormElement; // as HTMLBlaBlaElemnt will as IT type
